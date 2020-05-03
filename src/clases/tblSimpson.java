10029a6ab4f00f4d1f5f2774da4861279b3d8787
FernandoZib99 <bryanfernando.moxh.92@gmail.com>
2020-05-03 14:08:04 -0500

    Inclusión de tblSimpson y tblTrapecio

---
+++ src/clases/tblSimpson.java
@@ -0,0 +1,60 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package clases;
+
+import java.util.ArrayList;
+import javax.swing.table.AbstractTableModel;
+
+/**
+ *
+ * @author cecilio
+ */
+public class tblSimpson extends AbstractTableModel {
+
+    private String columnas []={"i","x","y"};
+    private ArrayList<csSimpson>lista=null;
+    
+     public tblSimpson(ArrayList<csSimpson> resultado) {
+        this.lista=resultado;
+    }
+     
+    
+    @Override
+    public int getRowCount() {
+                return this.lista.size();
+
+    }
+
+    
+    @Override
+    public int getColumnCount() {
+                return this.columnas.length;
+
+    }
+    
+    @Override
+    public Object getValueAt(int i, int columna) {
+        csSimpson fila = this.lista.get(i);
+        switch(columna){
+            case 0: return fila.getI();
+            case 1: return fila.getX();
+            case 2: return fila.getY();
+           
+        }
+        return null;
+    }
+    @Override
+     public boolean isCellEditable(int i, int i1) {
+        return true;
+    }
+
+    
+    @Override
+    public String getColumnName(int i) {
+        return this.columnas[i];
+    }
+    
+}
---
+++ src/clases/tblTrapecio.java
@@ -0,0 +1,60 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package clases;
+
+import java.util.ArrayList;
+import javax.swing.table.AbstractTableModel;
+
+/**
+ *
+ * @author cecilio
+ */
+public class tblTrapecio extends AbstractTableModel {
+    private String columnas[]={"i","(a+j)*h","fx"};
+     private ArrayList<csTrapecio> lista=null;
+
+    public tblTrapecio(ArrayList<csTrapecio> resultado) {
+        this.lista=resultado;
+    }
+     
+    
+    @Override
+    public int getRowCount() {
+                return this.lista.size();
+
+    }
+
+    
+    @Override
+    public int getColumnCount() {
+                return this.columnas.length;
+
+    }
+    
+    @Override
+    public Object getValueAt(int i, int columna) {
+        csTrapecio fila = this.lista.get(i);
+        switch(columna){
+            case 0: return fila.getj();
+            case 1: return fila.getAjh();
+            case 2: return fila.getFx();
+           
+        }
+        return null;
+    }
+    @Override
+     public boolean isCellEditable(int i, int i1) {
+        return true;
+    }
+
+    
+    @Override
+    public String getColumnName(int i) {
+        return this.columnas[i];
+    }
+     
+    
+}
