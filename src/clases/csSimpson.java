c5f18fb8a4581b239f4d658a0a303bbca757d1e9
FernandoZib99 <bryanfernando.moxh.92@gmail.com>
2020-05-03 14:21:40 -0500

    Inclusión de csSimpson y csTrapecio (Darwin)

---
+++ src/clases/csSimpson.java
@@ -0,0 +1,86 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package clases;
+
+/**
+ *
+ * @author cecilio
+ */
+public class csSimpson {
+    double Xi,Xf,h;
+    double x,y;
+    int i,n;
+    public csSimpson(double Xi, double Xf, int n) {
+        this.Xi = Xi;
+        this.Xf = Xf;
+        this.n = n;
+    }
+    public csSimpson(){
+    }
+
+    public double getXi() {
+        return Xi;
+    }
+
+    public void setXi(double Xi) {
+        this.Xi = Xi;
+    }
+
+    public double getXf() {
+        return Xf;
+    }
+
+    public void setXf(double Xf) {
+        this.Xf = Xf;
+    }
+
+    public double getH() {
+        return h;
+    }
+
+    public void setH(double h) {
+        this.h = h;
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public void setX(double x) {
+        this.x = x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public void setY(double y) {
+        this.y = y;
+    }
+
+    public int getI() {
+        return i;
+    }
+
+    public void setI(int i) {
+        this.i = i;
+    }
+
+    public int getN() {
+        return n;
+    }
+
+    public void setN(int n) {
+        this.n = n;
+    }
+
+    
+
+
+
+    
+    
+}
---
+++ src/clases/csTrapecio.java
@@ -0,0 +1,93 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package clases;
+
+/**
+ *
+ * @author cecilio
+ */
+public class csTrapecio  {
+    int j;
+    double ajh,fx ;
+    double fa,fb;
+    double a,b,n;
+    csTrapecio(){
+    }
+    csTrapecio(double a, double b,double n){
+        this.a=a;
+        this.b=b;
+        this.n=n;
+    }
+
+    public int getj() {
+        return j;
+    }
+
+    public void setJ(int j) {
+        this.j = j;
+    }
+
+    public double getAjh() {
+        return ajh;
+    }
+
+    public void setAjh(double ajh) {
+        this.ajh = ajh;
+    }
+
+    public double getFx() {
+        return fx;
+    }
+
+    public void setFx(double fx) {
+        this.fx = fx;
+    }
+
+    public double getFa() {
+        return fa;
+    }
+
+    public void setFa(double fa) {
+        this.fa = fa;
+    }
+
+    public double getFb() {
+        return fb;
+    }
+
+    public void setFb(double fb) {
+        this.fb = fb;
+    }
+
+    public double getA() {
+        return a;
+    }
+
+    public void setA(double a) {
+        this.a = a;
+    }
+
+    public double getB() {
+        return b;
+    }
+
+    public void setB(double b) {
+        this.b = b;
+    }
+
+    public double getN() {
+        return n;
+    }
+
+    public void setN(double n) {
+        this.n = n;
+    }
+    
+    
+    
+    
+    
+}
