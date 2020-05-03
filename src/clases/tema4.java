2c727c4ca768543ea827438ddff439bbd909ef5c
cecilioct <cecilio.cauichtun@itsva.edu.mx>
2020-05-03 13:45:26 -0600

    Las bases del software. Ventana principal y los jdg de cada método

---
+++ src/clases/tema4.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package clases;
+
+/**
+ *
+ * @author cecilio
+ */
+public class tema4 {
+    public static void main(String []args){
+        Principal ventana=new Principal();
+        ventana.setVisible(true);
+       
+    }
+    
+}
